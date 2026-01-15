6887863ff1192f225729938aa1e1ce0fe0a7a66e
AllenE <evangelista.davidallen2003@gmail.com>
2026-01-16 00:05:32 +0800

    updated favorite page

--- src/components/Favorite.tsx
+++ src/components/Favorite.tsx
@@ -2,6 +2,10 @@ function Favorite() {
   return (
     <div>
       <h2 className="font-semibold text-3xl py-10">Favorite Pokemons</h2>
+      <p>
+        Your Favorite Pokémon All the Pokémon you love, saved in one place.
+        Quickly revisit your favorites and explore their details anytime.
+      </p>
     </div>
   );
 }
