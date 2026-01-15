0e117dc388bba142c1d0bf18b6d3be6e0ff8eb72
AllenE <evangelista.davidallen2003@gmail.com>
2026-01-15 23:59:16 +0800

    updated home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,12 @@ function Home() {
   return (
     <div>
       <h2 className="font-semibold text-3xl py-10">Home</h2>
+      <p>
+        Welcome to the PokéDex App! Discover, explore, and learn about your
+        favorite Pokémon all in one place. Browse the complete Pokédex, check
+        Pokémon types and details, and save your favorites as you build your own
+        collection.
+      </p>
     </div>
   );
 }
